a29211b4a65167de00b735062249c4c3097061bc
lenlino <65199014+lenlino@users.noreply.github.com>
2025-01-30 11:46:13 +0900

    background.jsでのスクリプトとCSSの注入方法を更新し、manifest.jsonにscripting権限を追加

--- background.js
+++ background.js
@@ -1,19 +1,31 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if(tab.url.includes('/commits') && changeInfo.status === 'complete') {
-		if (tab.url.includes('/pull/')) {
-			injectPullRequestScripts(tabId);
-		} else {
+    if (tab.url.includes('/commits') && changeInfo.status === 'complete') {
+        if (tab.url.includes('/pull/')) {
+            injectPullRequestScripts(tabId);
+        } else {
             injectProjectScripts(tabId);
         }
-	}
+    }
 });
 
 function injectProjectScripts(tabId) {
-	chrome.tabs.executeScript(tabId, {file: "script.js"});
-	chrome.tabs.insertCSS(tabId, {file: "styles.css"});
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["script.js"]
+    });
+    chrome.scripting.insertCSS({
+        target: { tabId: tabId },
+        files: ["styles.css"]
+    });
 }
 
 function injectPullRequestScripts(tabId) {
-	chrome.tabs.executeScript(tabId, {file: "script.js"});
-	chrome.tabs.insertCSS(tabId, {file: "styles.css"});
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["script.js"]
+    });
+    chrome.scripting.insertCSS({
+        target: { tabId: tabId },
+        files: ["styles.css"]
+    });
 }
